533a6fcc5230596dbeaa83f6edfd99d12e7514b9
Software <111031492+CalTopSoft@users.noreply.github.com>
2026-04-19 14:21:12 -0500

    Delete StartCashFront directory

--- StartCashFront/src/services/debt.service.js
+++
@@ -1,9 +0,0 @@
-import { api } from '../api/client.js';
-
-export const debtService = {
-  // Obtiene todos los préstamos donde el usuario autenticado es el deudor (linkedUserId)
-  getMyDebts: () => api.get('/debts/mine'),
-
-  // Obtiene los pagos de un préstamo específico donde soy deudor
-  getPayments: (loanId) => api.get(`/debts/mine/${loanId}/payments`),
-};
--- StartCashFront/src/services/payment.service.js
+++
@@ -1,7 +0,0 @@
-import { api } from '../api/client.js';
-
-export const paymentService = {
-  listByLoan: (loanId) => api.get(`/payments/loan/${loanId}`),
-  create: (data) => api.post('/payments', data),
-  delete: (id) => api.delete(`/payments/${id}`),
-};
